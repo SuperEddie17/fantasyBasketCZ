1e3afa9c27f87d0d671aec288d36eb5a30c6cbe1
Alesek <acapek@email.cz>
2025-03-11 19:15:24 +0100

    text commit

---
+++ src/main/java/com/fantasy/fantasy_basket_service/FantasyBasketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fantasy.fantasy_basket_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyBasketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasyBasketServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fantasy/fantasy_basket_service/FantasyBasketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasy.fantasy_basket_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyBasketServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
